63beb07ddb4f224eecfa94fb361f197fa78b38ae
victorWeiFreelancer <victor.wei.freelancer@gmail.com>
2020-01-20 14:10:18 +0100

    small set working

--- practice/2008/qualification/TestCase.py
+++ practice/2008/qualification/TestCase.py
@@ -1,16 +1,37 @@
 class TestCase(object):
-    def __init__(self, number, nEngines, engines, nQuieries, queries, minSwitch=0):
+    def __init__(self, number, nEngines, engines, nQueries, queries, minSwitch=0):
         self.number = number
         self.nEngines = nEngines
         self.engines = engines
-        self.nQuieries = nQuieries
+        self.nQueries = nQueries
         self.queries = queries
         self.minSwitch = minSwitch
-    def findMinimalSwitch():
-        print("todo")
+    def findMinimalSwitch(self):
+        switchCount = 0
+        prevEngine = -1
+        vacancyList = [0]* (self.nEngines)
+        # print("Task %d engine vacant count %s" % (self.number, vacancyList))
+        if self.nQueries:
+            start = 0
+            end = self.nQueries
+            vacancyList = [0]*self.nEngines
+            while start<end:
+                vacancyList[self.queries[start]]+=1
+                if not any(v==0 for v in vacancyList) :
+                    # print("engine vacant count %s" % vacancyList)
+                    prevEngine=start
+                    switchCount+=1
+                    vacancyList = [0]*self.nEngines
+                    vacancyList[self.queries[start]]+=1
+                start+=1
+        self.minSwitch = switchCount
+        print("Case #%d: %d" %(self.number, self.minSwitch))
+        return switchCount
+
+
 
     def __repr__(self):
-        return "<Task %d:\n %d engines: %s\n %d queries:%s\n minimal switch:%d>" %(self.number, self.nEngines, self.engines, self.nQuieries, self.queries, self.minSwitch)
+        return "<Task %d:\n %d engines: %s\n %d queries:%s\n minimal switch:%d>" %(self.number, self.nEngines, self.engines, self.nQueries, self.queries, self.minSwitch)
     def __str__(self):
-        return "<Task %d:\n %d engines: %s\n %d queries:%s\n minimal switch:%d>" %(self.number, self.nEngines, self.engines, self.nQuieries, self.queries, self.minSwitch)
+        return "<Task %d:\n %d engines: %s\n %d queries:%s\n minimal switch:%d>" %(self.number, self.nEngines, self.engines, self.nQueries, self.queries, self.minSwitch)
     
--- practice/2008/qualification/solution.py
+++ practice/2008/qualification/solution.py
@@ -16,7 +16,11 @@ def main():
         for _ in range(nQuery):
             queryList.append( engineDict[input().rstrip('\n')] )
         testCaseList.append( TestCase(i+1, nEngine, engineDict, nQuery, queryList) )
-    print([x for x in testCaseList])
+        testCaseList[-1].findMinimalSwitch()
+    # testCaseList[6].findMinimalSwitch()
+    # print(testCaseList[6])
+    # print([x.minSwitch for x in testCaseList])
+    # print([x for x in testCaseList])
 
 if __name__ == '__main__':
     main()
