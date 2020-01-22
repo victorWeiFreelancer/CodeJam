daf18f63e7d7138d5803cdd57e73bad4988bacff
victorWeiFreelancer <victor.wei.freelancer@gmail.com>
2020-01-22 22:38:34 +0100

    b question about train time table done

---
+++ practice/2008/qualification/B-Train-table/b.py
@@ -0,0 +1,67 @@
+import sys
+# import numpy as np
+sys.dont_write_bytecode = True
+
+def timeStrConvertN(a):
+    h, m = list(map(int, a.split(':')))
+    return h*60+m
+
+def depart(deltaT, x):
+    # let the first train from x to depart
+    t = x[0][0][1]
+    iter = 1
+    x[0].pop(0)
+    while( len(x[iter])>0 and (t + deltaT <= x[iter][-1][0]) ):
+        for i in range( len(x[iter]) ):
+            if t+deltaT <= x[iter][i][0]:
+                # if find the first schedule that departs after arrival + turnaround
+                t = x[iter][i][1]
+                x[iter].pop(i)
+                iter = (iter + 1)%2
+                break
+
+def schedule(deltaT, a, b):
+    aCounter, bCounter, aPoint, bPoint = [0]*4
+    while(len(a) and len(b)):
+        if (a[0][0] < b[0][0]) or (a[0][0]==b[0][0] and a[0][1] <= b[0][1]):
+            aCounter+=1
+            # print("a depart %s" % a[0] )
+            depart(deltaT, [a, b])
+        elif (a[0][0] > b[0][0]) or ( a[0][0]==b[0][0] and a[0][1] > b[0][1] ):
+            bCounter+=1
+            # print("b depart %s" % b[0] )
+            depart(deltaT, [b, a])
+    if len(a):
+        # print("a remains")
+        aCounter += len(a)
+    elif len(b):
+        # print("b remains")
+        bCounter += len(b)
+    # print([aCounter, bCounter])
+    return [aCounter, bCounter]
+
+def main():
+    numTestCases = int(input())
+    for i in range(numTestCases):
+        turnAround = int(input())
+        na, nb = list(map(int, input().split()))
+        aTrips = []
+        bTrips = []
+        for _ in range(na):
+            startT, endT = input().split()
+            aTrips.append([timeStrConvertN(startT), timeStrConvertN(endT)])
+            aTrips = sorted(aTrips, key=lambda aTrips: aTrips[0])
+            
+            # aTrips.append([startT, endT])
+        for _ in range(nb):
+            startT, endT = input().split()
+            bTrips.append([timeStrConvertN(startT), timeStrConvertN(endT)])
+            bTrips = sorted(bTrips, key=lambda bTrips: bTrips[0])
+            # bTrips.append([startT, endT])
+        # print( aTrips )
+        # print( bTrips )
+        aTrains, bTrains = schedule(turnAround, aTrips, bTrips)
+        print( "Case #%d: %d %d" %(i+1, aTrains, bTrains))
+
+if __name__ == '__main__':
+    main()
